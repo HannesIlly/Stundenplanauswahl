774723636e2a5d91f8dbcccc2f0bad1280fb6a24
Hannes Illy <Hannes.Illy@gmx.de>
2018-08-02 02:05:15 +0200

    Added feature: Every command now ignores empty strings between two separators at every position of the command

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -6,7 +6,18 @@ public abstract class Command {
 
     public Command(String line, String separator) {
         this.line = line;
-        args = line.split(separator);
+        String[] arguments = line.split(separator);
+        String[] newArguments = new String[arguments.length];
+        int j = 0;
+        for (int i = 0; i < arguments.length; i++) {
+            if (arguments[i] != null && !arguments[i].equals("")) {
+                newArguments[j++] = arguments[i];
+            }
+        }
+        args = new String[j];
+        for (int i = 0; i < args.length; i++) {
+            args[i] = newArguments[i];
+        }
     }
 
     public String getLine() {
